30ac465f21f03cbaffc3abecd7c8db178ddc7de1
nataliamh.88@gmail.com <nataliamh.88@gmail.com>
2018-06-19 10:33:20 -0400

    Limpiado

--- ajax/assets/js/app.js
+++ ajax/assets/js/app.js
@@ -1,19 +1,12 @@
-//"http://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=0digIazhk4GYmGjbisFtCzQOEmm4wJzV&limit=5"
 const inputText = document.querySelector("input")
 const containerImage = document.getElementById("img")
 
 
 inputText.addEventListener("keypress", (event) => {
-    //ejecutamos una función con el elemento para llamarlo después
-   /* wich (le digo la tecla) y keyCode(le digo el código de la tecla) son eventos del teclado, 
-   con el código de abajo después le designamos el código de la tecla(13) 
-   para que sepa cuál es la tecla*/
-   let key = event.which || event.keyCode; //le di una condición de que se cumpla uno o(||) el otro
-   // 13 es enter, es su código del teclado
+   let key = event.which || event.keyCode; 
    if (key === 13) {
-       let search = inputText.value;//rescatamos valores con .value
-       //cuando aprieto enter le digo que me rescate el valor del input
-       inputText.value = ""; //para limpiar el input
+       let search = inputText.value;
+       inputText.value = ""; 
        fetch(`https://api.giphy.com/v1/gifs/search?api_key=0digIazhk4GYmGjbisFtCzQOEmm4wJzV&q=${search}&limit=25&offset=0&rating=G&lang=en`)
            .then(response => response.json())
            .then(data => {
@@ -29,4 +22,5 @@ inputText.addEventListener("keypress", (event) => {
         
         }); 
     }
+    
 
