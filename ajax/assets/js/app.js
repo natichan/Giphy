5b3cee7d7a1d0738d5ae764306de7c29dd9f3477
nataliamh.88@gmail.com <nataliamh.88@gmail.com>
2018-06-19 10:31:36 -0400

    Giphy funcionando

--- ajax/assets/js/app.js
+++ ajax/assets/js/app.js
@@ -13,22 +13,20 @@ inputText.addEventListener("keypress", (event) => {
    if (key === 13) {
        let search = inputText.value;//rescatamos valores con .value
        //cuando aprieto enter le digo que me rescate el valor del input
-       console.log(search)
        inputText.value = ""; //para limpiar el input
-
        fetch(`https://api.giphy.com/v1/gifs/search?api_key=0digIazhk4GYmGjbisFtCzQOEmm4wJzV&q=${search}&limit=25&offset=0&rating=G&lang=en`)
            .then(response => response.json())
            .then(data => {
-               console.log(data);
                renderInfo(data);
            })
    }
 })
 
-const renderInfo = data => {
-    containerImage.innerHTML = `<img src="${data.data.images.downsized_large.url}">` 
-    /*escribo la etiqueta imagen para que el HTML la lea, genero el source 
-    (donde la traigo por ejemplo data.Poster que contiene la imagen en el objeto de la API)*/
-    })
-    
-   }
+    const renderInfo = data =>{
+        data.data.forEach(element => {
+        let img = element.images.downsized.url;
+        containerImage.innerHTML += `<img src="${img}">`; 
+        
+        }); 
+    }
+
